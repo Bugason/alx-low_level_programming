5f8d04a3397dccba93168f6adf3a0644fd442cb1
Bugason <bugason.sam@gmail.com>
2023-03-16 05:19:07 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * prints - Programming is like building a multilingual puzzle
+ * main = entry point
+ * return = zero if succesful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
