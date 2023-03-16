da3e664ccd5ac126899afeaa6595c5ce6cb55203
Bugason <bugason.sam@gmail.com>
2023-03-16 06:18:55 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,15 +2,15 @@
 /**
 * print size - Prints size of size of 
 * main function = entry point
-* Return: always zero if succesful
+* Return:always zero if succesful
 */
 int main(void)
 {
-	printf("char size: %lu bytes\n", sizeof(char));
-	printf("int size: %lu bytes\n", sizeof(int));
-	printf("long int size: %lu bytes\n", sizeof(long int));
-	printf("long long int size: %lu bytes\n", sizeof(long long int));
-	printf("float size: %lu bytes\n", sizeof(float));
+	printf("char size: %lu byte(s)\n", sizeof(char));
+	printf("int size: %lu byte(s)\n", sizeof(int));
+	printf("long int size: %lu byte(s)\n", sizeof(long int));
+	printf("long long int size: %lu byte(s)\n", sizeof(long long int));
+	printf("float size: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
 /**
