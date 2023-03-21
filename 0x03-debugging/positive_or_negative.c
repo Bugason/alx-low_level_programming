00b8d744e96656be3f12acfecacaaac50b0d0b9c
Bugason <bugason.sam@gmail.com>
2023-03-21 02:34:17 -0700

    main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * void positive_or_negative - entry point
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d is positive\n", i);
+	else if (i < 0)
+		printf("%d is negative\n", I);
+	else
+		printf("%d is zero\n", i);
+}
