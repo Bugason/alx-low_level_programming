2a9f64b61cdd38cf76dc81228cf9f190b045fe7d
Bugason <bugason.sam@gmail.com>
2023-03-23 02:53:24 -0700

    multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ *  mul - multiples two intergers
+ *  @a: first integer
+ *  @b: second integer
+ *
+ *  Return: addition of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
